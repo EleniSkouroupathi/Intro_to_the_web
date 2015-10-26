b571a950555d875d2c0c8ab76c830ef5e26033e3
Eleni Skouroupathi <eleni59@hotmail.com>
2015-10-26 17:01:32 +0000

    Chapter 8

--- app.rb
+++ app.rb
@@ -3,3 +3,13 @@ require 'sinatra'
 get '/' do
   "hello!"
 end
+
+get '/secret' do
+	"how are you?"
+end
+
+get '/cat' do
+     "<div style='border: 3px dashed red'>
+     <img src='http://bit.ly/1eze8aE'>
+   </div>"
+end
